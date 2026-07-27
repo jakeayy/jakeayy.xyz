41ef33f7c7b7658bc886801d58296dfbc3519a85
jakeayy <tools.crumb695@passinbox.com>
2026-07-27 13:24:15 +0200

    added security headers

--- svelte.config.js
+++ svelte.config.js
@@ -12,6 +12,25 @@ const config = {
 		adapter: adapter(),
 		alias: {
 			"@/*": join(import.meta.dirname, "src", "*")
+    },
+		csp: {
+      mode: "hash",
+      directives: {
+          "default-src": ["self"],
+          "script-src": [
+              "self",
+              "https://keepandroidopen.org" // src/routes/blog/[slug]/+page.svelte
+          ],
+          // shiki and svelte style need inlines
+          "style-src-attr": ["unsafe-inline"],
+          "style-src-elem": ["self"],
+          "img-src": ["self", "data:"],
+          "font-src": ["self", "data:"],
+          "connect-src": ["self"],
+          "object-src": ["none"],
+          "base-uri": ["self"],
+          "form-action": ["self"]
+      }
 		},
 		prerender: {
 			handleUnseenRoutes({ message, routes }) {
