8d351421cca9bb99b26fd4a5444123da5040ef71
jakeayy <tools.crumb695@passinbox.com>
2026-08-04 02:18:57 +0200

    fix post checker from crashing on invalid post metadata

--- src/lib/blog-meta.server.ts
+++ src/lib/blog-meta.server.ts
@@ -2,7 +2,7 @@ import { dev } from "$app/environment";
 import type { PostMetadata } from "$lib/types/blog";
 
 export function isValidPost(meta: Partial<PostMetadata>): meta is PostMetadata {
-    return typeof meta.title === "string"
+    return meta && typeof meta.title === "string"
 		&& typeof meta.published === "boolean"
 		&& typeof meta.date === "string"
         && (meta.published || dev)
